f8261dd2147bbfb31d7d3d8b1c391c527f153cca
DraeRamsey <andrea.jos.ramsey@gmail.com>
2021-03-22 12:59:05 -0700

    add JavaScript, CSS tweaks, media queries, meta data, update README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Fluorescent Coding Challenge
+
+This is a take-home challenge for Fluorescent Design Inc.
+Please view the finished page [here](http://www.google.com)
+
+## Overview
+
+The challenge was to develop a piece of a theme product page based on a design prototype and a list of requirements.
+
+## Notes
+
+For this challenge I focused more on getting the design as close to the prototype as possible/ following the requirements and less on developing dynamically created elements which would be the next step.  
+
+### Stack
+
+CSS, HTML, JavaScript, jQuery
--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+$(document).ready(function() {
+
+
+  let products = ["Red", "Green", "Blue"]
+
+  $('.product__color-btn').click(function() {
+
+    let product_index = $(this).data("index");
+    $('#title').text(products[product_index] + " Flyknit Trainers")
+
+    toggleActive();
+
+
+  });
+
+
+  function toggleActive() {
+    const active = $('.product__color-btn--active');
+
+    if (active) {
+      $(active).removeClass('product__color-btn--active');
+    }
+
+    const new_active = $(this);
+    $(new_active).addClass('product__color-btn--active');
+  }
+
+
+});
