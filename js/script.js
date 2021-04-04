22a62bc341ab0f213d3c264d4e244eb833d34733
DraeRamsey <andrea.jos.ramsey@gmail.com>
2021-04-03 19:51:19 -0700

    show only the active colour variant

--- js/script.js
+++ js/script.js
@@ -2,28 +2,31 @@
 
 $(document).ready(function() {
 
-
   let products = ["Red", "Green", "Blue"];
+  let product_color = products[0].toLowerCase();
+  let active_color = product_color;
 
-
+  $('#' + product_color).css("display", "block");
 
   $('.product__color-btn').click(function() {
 
     let product_index = $(this).data("index");
+    let active_btn = $('.product__color-btn--active');
+    product_color = products[product_index].toLowerCase();
 
     $('#title').text(products[product_index] + " Flyknit Trainers")
-    const active = $('.product__color-btn--active');
 
-    if (active) {
-      $(active).removeClass('product__color-btn--active');
+    if (active_btn) {
+      $(active_btn).removeClass('product__color-btn--active');
+      $('#' + active_color).css("display", "none");
     }
 
-    const new_active = $(this);
-    $(new_active).addClass('product__color-btn--active');
+    let new_active_btn = $(this);
+    $(new_active_btn).addClass('product__color-btn--active');
+    $('#' + product_color).css("display", "block");
+    active_color = product_color;
 
   });
-
-
 
 
 });
